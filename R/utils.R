0092970359770cc3ad77632e86599263fb5de0e0
Julian Karch <karch@mpib-berlin.mpg.de>
2017-06-29 16:04:03 +0200

    removed simulate; debugging it elsewhere

--- R/utils.R
+++ R/utils.R
@@ -1,29 +1,5 @@
 #simulate Data for on OpenMx Model
 #' @export
-simulateData <- function(run,N=1){
-  stopifnot(class(run)=='MxModel' |class(run)=='MxRAMModel')
-  manifests <- run@manifestVars
-
-  #add fake data
-  fake.data <- data.frame(matrix(1:(length(manifests)*2),ncol=length(manifests)))
-  names(fake.data)<- manifests
-  run@data <- mxData(fake.data,type="raw")
-
-  #run to get exp mean and covariance
-  fit <- mxRun(run,useOptimizer=F,silent=T)
-  covM <- fit$fitfunction@info$expCov
-  mu <- fit$fitfunction@info$expMean
-
-  #generate Data
-  simData <- mvrnorm(n=N, mu, covM)
-  if (N==1){
-    simData <- t(simData)
-  }
-  colnames(simData) <- manifests
-  result <- data.frame(Time=run$data@observed[1,paste0('time',1:length(manifests))],Y=t(simData))
-  return(result)
-}
-
 plotSEM <- function(simData,N=1){
   simDataLong <- melt(simData, id="Time")
   print(ggplot(simDataLong,aes(x=Time,y=value,colour=variable)) + geom_line())
