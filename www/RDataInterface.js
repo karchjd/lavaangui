7a98d90b42cd909b0c013af75b3f35a3c8a095d8
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2023-06-15 16:46:27 +0200

    only check connection when loading data if there are manifest variables in the model

--- www/RDataInterface.js
+++ www/RDataInterface.js
@@ -1,17 +1,18 @@
 
 Shiny.addCustomMessageHandler('columnNames', function (columnNames) {
-    applyLinkedClass(columnNames);
-    appState.setColumnNamesGlobal(columnNamesGlobal);
+  applyLinkedClass(columnNames);
+  appState.setColumnNamesGlobal(columnNamesGlobal);
 });
 
 Shiny.addCustomMessageHandler('fname', function (fname) {
-    appState.getLoadedFileName(fname);
+  appState.getLoadedFileName(fname);
 });
 
 function applyLinkedClass(columnNames) {
-    const nodes = cy.nodes(function(node){
-        return node.hasClass("observed-variable")
-    });
+  const nodes = cy.nodes(function (node) {
+    return node.hasClass("observed-variable")
+  });
+  if (nodes.length > 0) {
     let all_linked = true;
     for (var i = 0; i < nodes.length; i++) {
       const node = nodes[i];
@@ -19,12 +20,12 @@ function applyLinkedClass(columnNames) {
       node.removeClass("linked")
       if (columnNames.includes(label)) {
         node.addClass("linked");
-      }else{
+      } else {
         all_linked = false;
       }
     }
-    if(all_linked){
-        alert("All observed variables were linked with data")
+    if (all_linked) {
+      alert("All observed variables were linked with data")
     }
   }
-  
+}
