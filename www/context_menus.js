6ab7546957205b23601f9fb5788fb623c6b572ee
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2023-06-15 10:45:13 +0200

    made rename popup more user-friendly

--- www/context_menus.js
+++ www/context_menus.js
@@ -192,111 +192,145 @@ cy.contextMenus({
         },
         {
             id: 'rename-node',
-            content: 'Rename Variable',
-            selector: 'node.latent-variable, node.observed-variable',
-            onClickFunction: function (event) {
-                var node = event.target || event.cyTarget;
-                var currentLabel = node.data('label');
+content: 'Rename Variable',
+selector: 'node.latent-variable, node.observed-variable',
+onClickFunction: function (event) {
+    var node = event.target || event.cyTarget;
+    var currentLabel = node.data('label');
 
-                // Create a backdrop
-                var backdrop = document.createElement('div');
-                backdrop.style.position = 'fixed';
-                backdrop.style.top = 0;
-                backdrop.style.left = 0;
-                backdrop.style.width = '100vw';
-                backdrop.style.height = '100vh';
-                backdrop.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'; // semi-transparent
-                backdrop.style.zIndex = 1000; // ensure it's on top
+    // Create a backdrop
+    var backdrop = document.createElement('div');
+    backdrop.style.position = 'fixed';
+    backdrop.style.top = 0;
+    backdrop.style.left = 0;
+    backdrop.style.width = '100vw';
+    backdrop.style.height = '100vh';
+    backdrop.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'; // semi-transparent
+    backdrop.style.zIndex = 1000; // ensure it's on top
 
-                // Create a modal
-                var modal = document.createElement('div');
-                modal.style.position = 'fixed';
-                modal.style.top = '50%';
-                modal.style.left = '50%';
-                modal.style.transform = 'translate(-50%, -50%)';
-                modal.style.backgroundColor = 'white';
-                modal.style.padding = '20px';
-                modal.style.border = '1px solid #ccc';
-                modal.style.zIndex = 1001; // ensure it's above the backdrop
+    // Create a modal
+    var modal = document.createElement('div');
+    modal.style.position = 'fixed';
+    modal.style.top = '50%';
+    modal.style.left = '50%';
+    modal.style.transform = 'translate(-50%, -50%)';
+    modal.style.backgroundColor = 'white';
+    modal.style.padding = '20px';
+    modal.style.border = '1px solid #ccc';
+    modal.style.zIndex = 1001; // ensure it's above the backdrop
 
-                var select = false;
-                // Add select box if columnNamesGlobal is defined
-                if (typeof columnNamesGlobal !== 'undefined' && node.hasClass('observed-variable')) {
-                    select = true;
-                    // Add label for the select box
-                    var selectLabel = document.createElement('label');
-                    selectLabel.textContent = "Available Variables";
-                    selectLabel.setAttribute('for', 'variables-select');
-                    selectLabel.style.marginRight = '10px'; // Add margin to the right side of the label
-                    modal.appendChild(selectLabel);
+    var select = false;
+    // Add select box if columnNamesGlobal is defined
+    if (typeof columnNamesGlobal !== 'undefined' && node.hasClass('observed-variable')) {
+        select = true;
+        // Add label for the select box
+        var selectLabel = document.createElement('label');
+        selectLabel.textContent = "Available Variables";
+        selectLabel.setAttribute('for', 'variables-select');
+        selectLabel.style.marginRight = '10px'; // Add margin to the right side of the label
+        modal.appendChild(selectLabel);
 
-                    var selectBox = document.createElement('select');
-                    selectBox.id = 'variables-select';
-                       // Get all current node labels
-    var currentLabels = cy.nodes().map(node => node.data('label'));
+        var selectBox = document.createElement('select');
+        selectBox.id = 'variables-select';
+        // Get all current node labels
+        var currentLabels = cy.nodes().map(node => node.data('label'));
 
-    columnNamesGlobal.forEach(function (columnName) {
-        // Only add as option if not already a node label
-        if (!currentLabels.includes(columnName)) {
-            var option = document.createElement('option');
-            option.value = columnName;
-            option.textContent = columnName;
-            selectBox.appendChild(option);
+        columnNamesGlobal.forEach(function (columnName) {
+            // Only add as option if not already a node label
+            if (!currentLabels.includes(columnName)) {
+                var option = document.createElement('option');
+                option.value = columnName;
+                option.textContent = columnName;
+                selectBox.appendChild(option);
+            }
+        });
+        modal.appendChild(selectBox);
+        var lineBreak = document.createElement('br');
+        modal.appendChild(lineBreak);
+    }
+
+    var inputLabel = document.createElement('label');
+    inputLabel.style.marginRight = '10px'; // Add margin to the right side of the label
+    inputLabel.textContent = "New label";
+    inputLabel.setAttribute('for', 'new-label-input');
+    modal.appendChild(inputLabel);
+
+    // Add input field under the select box
+    var inputField = document.createElement('input');
+    inputField.type = 'text';
+    inputField.value = ""; // set the default value as empty
+    inputField.id = "rename-input"
+    modal.appendChild(inputField);
+
+    var lineBreak = document.createElement('br');
+    modal.appendChild(lineBreak);
+    
+    // Create a submit button
+    var submitButton = document.createElement('button');
+    submitButton.textContent = 'Submit';
+    modal.appendChild(submitButton);
+
+    // Create a cancel button
+    var cancelButton = document.createElement('button');
+    cancelButton.textContent = 'Cancel';
+    cancelButton.style.marginLeft = '10px';
+    modal.appendChild(cancelButton);
+
+    // Add the backdrop and modal to the document
+    document.body.appendChild(backdrop);
+    document.body.appendChild(modal);
+
+    //focus input field
+    var field = document.getElementById("rename-input");
+    field.focus();
+
+
+    // Handle the submit button click
+    submitButton.addEventListener('click', function () {
+        handleSubmit();
+    });
+
+    // Handle the cancel button click
+    cancelButton.addEventListener('click', function () {
+        close();
+    });
+
+    // Handle the Enter key in the input field
+    inputField.addEventListener('keydown', function (e) {
+        if (e.key === 'Enter') {
+            handleSubmit();
         }
     });
-                    modal.appendChild(selectBox);
-                    var lineBreak = document.createElement('br');
-                    modal.appendChild(lineBreak);
+
+    function handleSubmit() {
+        if (inputField.value !== '' || select) {
+            var newLabel;
+            if (inputField.value !== '') {
+                newLabel = inputField.value;
+            } else {
+                newLabel = selectBox.value;
+            }
+            node.data('label', newLabel);
+
+            if (node.hasClass('observed-variable')) {
+                if (columnNamesGlobal && columnNamesGlobal.includes(newLabel)) {
+                    node.addClass('linked');
+                    alert('Variable connected with data set');
+                } else if (node.hasClass('linked')) {
+                    node.removeClass('linked');
+                    alert('Variable disconnected');
                 }
+            }
+        }
+        close();
+    }
+    function close() {
+        // Remove the modal and backdrop from the document
+        document.body.removeChild(modal);
+        document.body.removeChild(backdrop);
+    }
+},
 
-                var inputLabel = document.createElement('label');
-                inputLabel.style.marginRight = '10px'; // Add margin to the right side of the label
-                inputLabel.textContent = "New label";
-                inputLabel.setAttribute('for', 'new-label-input');
-                modal.appendChild(inputLabel);
-
-                // Add input field under the select box
-                var inputField = document.createElement('input');
-                inputField.type = 'text';
-                inputField.value = ""; // set the default value as empty
-                modal.appendChild(inputField);
-
-
-                // Create a submit button
-                var submitButton = document.createElement('button');
-                submitButton.textContent = 'Submit';
-                modal.appendChild(submitButton);
-
-                // Add the backdrop and modal to the document
-                document.body.appendChild(backdrop);
-                document.body.appendChild(modal);
-
-                // Handle the submit button click
-                submitButton.addEventListener('click', function () {
-                    if(inputField.value !== '' || select){
-                        var newLabel
-                        if(inputField.value !== ''){
-                            newLabel = inputField.value;
-                        }else{
-                            newLabel = selectBox.value;
-                        }
-                        node.data('label', newLabel);
-    
-                        if (node.hasClass('observed-variable')) {
-                            if (columnNamesGlobal && columnNamesGlobal.includes(newLabel)) {
-                                node.addClass('linked');
-                                alert('Variable connected with data set');
-                            } else if (node.hasClass('linked')) {
-                                node.removeClass('linked');
-                                alert('Variable disconnected');
-                            }
-                        }    
-                    }
-                    // Remove the modal and backdrop from the document
-                    document.body.removeChild(modal);
-                    document.body.removeChild(backdrop);
-                });
-            },
         },
         {
             id: 'remove-node',
