c2bcf294dc0e4b1144e1842e525f0bd269c7ef7d
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2023-06-20 16:01:06 +0200

    made help menu prettier

--- www/mainMenus.js
+++ www/mainMenus.js
@@ -5,7 +5,7 @@ document.querySelectorAll('.menu > ul > li').forEach(item => {
         const submenu = item.querySelector('.submenu');
         if (submenu) {
             submenu.style.display = 'block';
-            
+
             // Additionally set display block to submenu's li and a elements
             const submenuItems = submenu.querySelectorAll('li, a');
             submenuItems.forEach(submenuItem => {
@@ -28,49 +28,96 @@ document.querySelectorAll('.menu > ul > li').forEach(item => {
     });
 });
 
-document.getElementById("help-menu-item").addEventListener('click', function(event) {
+
+// Add an event listener to the help menu item
+$("#help-menu-item").click(function (event) {
     event.preventDefault(); // Prevent the default action of the link
 
     // Help content
     var helpContent = [
-        "Hold command or ctrl: draw directed arrows",
-        "Hold space: draw undirected arrows (currently disabled because of bug)",
-        "Backspace: delete selected elements",
-        "o: create observed variable at mouse location",
-        "l: create latent variable at mouse location",
-        "c: create constant variable at mouse location"
-    ].join('\n'); // Join the array into a single string, separating each item with a newline character
+        { command: "Hold command/ctrl:", description: "Draw directed arrows" },
+        { command: "Hold space:", description: "Draw undirected arrows (currently disabled because of bug)" },
+        { command: "Backspace:", description: "Delete selected elements" },
+        { command: "o:", description: "Create observed variable at mouse location" },
+        { command: "l:", description: "Create latent variable at mouse location" },
+        { command: "c:", description: "Create constant variable at mouse location" }
+    ];
 
-    // Show the alert
-    alert(helpContent);
+    // Display the modal dialog
+    var modal = $("#help-modal");
+    var helpContentElement = $("#help-content");
+    helpContentElement.html(generateHelpList(helpContent));
+
+    // Show the modal dialog
+    modal.css("display", "flex")
+    modal.show();
+
+    // Set focus on the modal content
+    $(".modal-content").focus();
 });
 
+$(document).on("keydown", function(event) {
+    if (event.key === "Escape") {
+      $("#help-modal").hide();
+    }
+});
+
+// Close the modal dialog when the close button is clicked
+$(document).on("click", ".close", function () {
+    $("#help-modal").hide();
+});
+
+// Close the modal dialog when the user clicks outside of it
+$(document).on("click", function (event) {
+    if ($(event.target).is("#help-modal")) {
+        $("#help-modal").hide();
+    }
+});
+
+// Prevent focus from going outside of the modal content
+$(document).on("focusin", function (event) {
+    if ($("#help-modal").is(":visible") && !$(event.target).closest(".modal-content").length) {
+        event.preventDefault();
+        $(".modal-content").focus();
+    }
+})
+
+function generateHelpList(contentArray) {
+    var helpList = "";
+    for (var i = 0; i < contentArray.length; i++) {
+      var item = contentArray[i];
+      helpList += '<div class="help-item"><strong>' + item.command + '</strong><span>' + item.description + '</span></div>';
+    }
+    return helpList;
+  }
 
 
 
-function handleItemChange(item) {
-    // Build the string based on selected items
-    let outputString = 'Hallo';
 
-    const wordMapping = {
-        'Show Estimate': 'world',
-        'Show pvalue': 'suckers,',
-        'Show standard error': 'Each',
-        'Show confidence interval': 'item'
-    };
 
-    document.querySelectorAll('.checkmark-container').forEach(item => {
-        let label = item.querySelector('label').textContent;
-        let isSelected = item.getAttribute('data-selected') === 'true';
+    function handleItemChange(item) {
+        // Build the string based on selected items
+        let outputString = 'Hallo';
 
-        if (isSelected) {
-            outputString += ' ' + wordMapping[label];
-        }
-    });
+        const wordMapping = {
+            'Show Estimate': 'world',
+            'Show pvalue': 'suckers,',
+            'Show standard error': 'Each',
+            'Show confidence interval': 'item'
+        };
 
-    // Set the content of the output element to the generated string
-    console.log(outputString)
-}
+        document.querySelectorAll('.checkmark-container').forEach(item => {
+            let label = item.querySelector('label').textContent;
+            let isSelected = item.getAttribute('data-selected') === 'true';
+
+            if (isSelected) {
+                outputString += ' ' + wordMapping[label];
+            }
+        });
+
+        // Set the content of the output element to the generated string
+        console.log(outputString)
+    }
 
 
 
