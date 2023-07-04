107fc2907c1d7c91e7d729d31649cbeb7511f46d
j.d.karch@fsw.leidenuniv.nl <j.d.karch@fsw.leidenuniv.nl>
2023-07-04 16:09:24 +0200

    still running

--- src/Graph/InitGraph.svelte
+++ src/Graph/InitGraph.svelte
@@ -2,11 +2,12 @@
   import cytoscape from "cytoscape";
   import GraphStyles from "./GraphStyles.js";
   import { cyStore } from "../stores.js";
+  import edgehandles from "cytoscape-edgehandles";
+  cytoscape.use(edgehandles);
   var cy = cytoscape({
     autoungrabify: false,
     autolock: false,
     style: GraphStyles,
   });
   cyStore.set(cy);
-  console.log("init");
 </script>
--- src/stores.js
+++ src/stores.js
@@ -1,2 +1,3 @@
 import { writable } from 'svelte/store';
 export const cyStore = writable(0);
+export const ehShore = writable(0);
