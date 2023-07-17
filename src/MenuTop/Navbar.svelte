412e10a47ce89022faa699d7fb43fc8c1955a0a7
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2023-07-17 13:24:13 +0200

    disable top menu collapse

--- src/MenuTop/Navbar.svelte
+++ src/MenuTop/Navbar.svelte
@@ -4,11 +4,9 @@
 </script>
 
 <nav class="navbar navbar-default navbar-fixed-top navbar-left">
-  <div class="container">
     <ul class="nav navbar-nav navbar-left">
       <slot />
     </ul>
-  </div>
 </nav>
 
 <style>
