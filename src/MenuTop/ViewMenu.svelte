665fdefd1e0e6ed2c641a2e6ce8f7a1c71bf0d69
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2023-07-17 17:55:10 +0200

    View menu works

--- src/MenuTop/ViewMenu.svelte
+++ src/MenuTop/ViewMenu.svelte
@@ -1,10 +1,35 @@
 <script>
     import DropDownCheck from "./helpers/DropDownCheck.svelte";
+    import { cyStore } from "../stores";
+    import { get } from "svelte/store";
+
     let menuItems = [
-      { name: "Edges Created by Lavaan", checked: true},
-      { name: "Variance Edges", checked: true},
+      { name: "Edges Created by Lavaan", checked: true, class: "fromLav"},
+      { name: "Variance Edges", checked: true, class: "loop"},
     ];
+
+    $: {
+      updateVisibility(menuItems);
+    }
+
+    function updateVisibility(menuItems){
+        const cy = get(cyStore);
+        console.log("update")
+        if(menuItems[0].checked && menuItems[1].checked){
+            cy.elements("."+ menuItems[0].class).show();
+            cy.elements("."+ menuItems[1].class).show();
+        }else if(menuItems[0].checked && !menuItems[1].checked){
+            cy.elements("."+ menuItems[0].class).show();
+            cy.elements("."+ menuItems[1].class).hide();
+        }else if(!menuItems[0].checked && menuItems[1].checked){
+            cy.elements("."+ menuItems[1].class).show();
+            cy.elements("."+ menuItems[0].class).hide();
+        }else{
+            cy.elements("."+ menuItems[1].class).hide();
+            cy.elements("."+ menuItems[0].class).hide();
+        }
+    }
   </script>
   
-  <DropDownCheck name={"View"} {menuItems} />
+  <DropDownCheck name={"View"} bind:menuItems={menuItems} />
   
