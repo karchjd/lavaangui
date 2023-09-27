d96ef8dd620b90b799c55f6bb0ea477f2a04ded3
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2023-09-27 19:22:48 +0200

    massive improvements to checkNodeLoop, at the moment the version that first checks whether default positions are free.

--- src/Graph/checkNodeLoop.js
+++ src/Graph/checkNodeLoop.js
@@ -1,7 +1,141 @@
 export function checkNodeLoop(nodeID) {
-  const action = countEdgesConnectedToNodeSides(nodeID);
-  moveLoopNode(nodeID, action);
+  const selfLoop = getSelfLoop(nodeID);
+  if (selfLoop != null) {
+    const edgePostions = getEdgePositions(nodeID);
+    const angles = getOccupiedAngles(edgePostions);
+    const freeDefault = defaultPositionsFree(angles);
+    let goalAngle;
+    if (freeDefault.length > 1) {
+      if (freeDefault.includes("top")) {
+        goalAngle = 0;
+      } else if (freeDefault.includes("bottom")) {
+        goalAngle = 180;
+      } else if (freeDefault.includes("right")) {
+        goalAngle = 90;
+      } else if (freeDefault.includes("left")) {
+        goalAngle = 270;
+      }
+    } else {
+      goalAngle = getBestFreeAngle(angles);
+    }
+
+    selfLoop.style("loop-direction", `${goalAngle}deg`);
+  }
 }
+
+function defaultPositionsFree(angles) {
+  const intervals = {
+    top: [315, 45],
+    right: [45, 135],
+    bottom: [135, 225],
+    left: [225, 315],
+  };
+
+  let freeIntervals = [];
+
+  for (let key in intervals) {
+    const [start, end] = intervals[key];
+    let isFree = true;
+
+    for (const angle of angles) {
+      const normalizedAngle = ((angle % 360) + 360) % 360;
+      if (start <= end) {
+        if (normalizedAngle >= start && normalizedAngle <= end) {
+          isFree = false;
+          break;
+        }
+      } else {
+        if (normalizedAngle >= start || normalizedAngle <= end) {
+          isFree = false;
+          break;
+        }
+      }
+    }
+
+    if (isFree) {
+      freeIntervals.push(key);
+    }
+  }
+
+  return freeIntervals;
+}
+
+function getEdgePositions(nodeID) {
+  const node = cy.getElementById(nodeID);
+  if (node.length === 0) {
+    console.error(`Node with ID ${nodeID} does not exist.`);
+    return;
+  }
+  let edgePostions = [];
+  debugger;
+  node.connectedEdges().forEach((edge) => {
+    const source = edge.source();
+    const target = edge.target();
+    let toAdd;
+    if (source.id() !== node.id() || target.id() !== node.id()) {
+      if (source.id() === node.id()) {
+        console.log("sourceEndpointAdded");
+        toAdd = elementWiseSubtract(edge.sourceEndpoint(), node.position());
+      } else {
+        toAdd = elementWiseSubtract(edge.targetEndpoint(), node.position());
+      }
+      edgePostions.push(toAdd);
+    }
+  });
+  return edgePostions;
+}
+
+function elementWiseSubtract(obj1, obj2) {
+  return Object.keys(obj1).reduce((acc, key) => {
+    if (obj2.hasOwnProperty(key)) {
+      acc[key] = obj1[key] - obj2[key];
+    } else {
+      throw new Error(`Key ${key} not found in second object`);
+    }
+    return acc;
+  }, {});
+}
+
+function getOccupiedAngles(edgePostions) {
+  let angles = [];
+  debugger;
+  edgePostions.forEach((position) => {
+    angles.push(getAngleFromPos(position));
+  });
+  console.log(angles);
+  return angles;
+}
+
+function getAngleFromPos(position) {
+  let angle = (Math.atan2(position.y, position.x) * 180) / Math.PI;
+  angle = (angle + 90) % 360;
+  return angle;
+}
+
+function getBestFreeAngle(angles) {
+  let maxDistance = -1;
+  let furthestDegree = null;
+
+  for (let i = 0; i <= 360; i++) {
+    let minDistance = Infinity;
+
+    for (let j = 0; j < angles.length; j++) {
+      let d = Math.abs(angles[j] - i);
+      // Adjust distance considering that 0 and 360 are the same
+      d = Math.min(d, 360 - d);
+
+      minDistance = Math.min(minDistance, d);
+    }
+
+    if (minDistance > maxDistance) {
+      maxDistance = minDistance;
+      furthestDegree = i;
+    }
+  }
+  return furthestDegree;
+}
+
+function moveEdge(edge, angle) {}
 
 function countEdgesConnectedToNodeSides(nodeId) {
   const node = cy.getElementById(nodeId);
@@ -16,20 +150,6 @@ function countEdgesConnectedToNodeSides(nodeId) {
   const nodePosition = node.position();
   const nodeHeight = node.height();
 
-  node.connectedEdges().forEach((edge) => {
-    const source = edge.source();
-    const target = edge.target();
-    const otherNode = source.id() === node.id() ? target : source;
-    const otherNodePosition = otherNode.position();
-    if (node.id() !== otherNode.id()) {
-      if (otherNodePosition.y < nodePosition.y - nodeHeight / 2) {
-        topCount++;
-      } else if (otherNodePosition.y > nodePosition.y + nodeHeight / 2) {
-        bottomCount++;
-      }
-    }
-  });
-
   if (topCount > 0 && bottomCount === 0) {
     return "bottom";
   }
@@ -41,7 +161,7 @@ function countEdgesConnectedToNodeSides(nodeId) {
 
 // Rest of the functions remain unchanged
 
-function moveLoopNode(nodeId, action) {
+function getSelfLoop(nodeId, angle) {
   var node = cy.getElementById(nodeId);
   var loopEdge = null;
 
@@ -54,17 +174,5 @@ function moveLoopNode(nodeId, action) {
       return;
     }
   });
-  moveLoopEdge(loopEdge, action);
-}
-
-// Function to move edge loops to the bottom of nodes
-function moveLoopEdge(selfEdge, action) {
-  if (selfEdge != null) {
-    var currentLoopDirection = selfEdge.style("loop-direction");
-    if (action == "bottom" && currentLoopDirection == "0rad") {
-      selfEdge.style("loop-direction", "3.14");
-    } else if (action == "top" && currentLoopDirection == "3.14rad") {
-      selfEdge.style("loop-direction", "0");
-    }
-  }
+  return loopEdge;
 }
