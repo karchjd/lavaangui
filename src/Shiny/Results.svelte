21fa20d40b6478ac353d830342862d1a25efb6b7
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2023-10-11 20:32:37 +0200

    show help in syntax menu

--- src/Shiny/Results.svelte
+++ src/Shiny/Results.svelte
@@ -1,4 +1,22 @@
-<pre id="lavaan_syntax_R" class="shiny-html-output well" />
+<pre id="lavaan_syntax_R" class="shiny-html-output well">
+Command               Action
+--------------------------------------------------
+Right-Click           Right-Click Anywhere to get an Appropriate Menu 
+o                     Create Observed Variable at Mouse Location
+l                     Create Latent Variable at Mouse Location
+c                     Create Constant Variable at Mouse Location
+Hold Shift            Draw Undirected Arrows by Connecting Variables With Mouse
+Hold CTRL             Draw Directed Arrows by Connect Variables With Mouse
+Hold CTRL             Click on Multiple Elements to Select
+Hold CTRL             Click on Canvas to Activate Select Box
+Backspace             Remove Selected Elements
+CTRL+Z                Undo
+CTRL+Y                Redo
+
+
+Mac Users Replace CTRL with CMD
+
+</pre>
 
 <style>
   pre {
