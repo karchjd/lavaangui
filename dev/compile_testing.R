555f615724f0e31617de19090ac9a9c61ff0419b
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2023-10-24 01:38:11 +0200

    debug workflow

---
+++ dev/compile_testing.R
@@ -0,0 +1,19 @@
+recompile_front <- T
+
+if (recompile_front){
+  # Compile Svelte front end 
+  ret_val <- system("cd src && npm run build")
+  if (ret_val != 0) {
+    stop("Failed to compile Svelte front end.")
+  }  
+}
+
+
+# Detach all loaded packages and clean your environment
+golem::detach_all_attached()
+
+# Document and reload your package
+#roxygen2::roxygenise()
+pkgload::load_all()
+options(shiny.port = 3245)
+start_gui()
