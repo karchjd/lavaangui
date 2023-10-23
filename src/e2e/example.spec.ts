25e21aa869ea42731a32a4b2d3dc482d9cf84054
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2023-10-23 23:16:12 +0200

    testing github actions

--- src/e2e/example.spec.ts
+++ src/e2e/example.spec.ts
@@ -1,13 +1,32 @@
 import { test, expect } from "@playwright/test";
 
-test("new model", async ({ page }) => {
-  await page.goto("http://127.0.0.1:3245/");
-  await page.getByRole("button", { name: "File" }).click();
-  await page.getByRole("link", { name: "New Model" }).click();
-  await page.getByText("OK").click();
-  const numberOfNodes = await page.evaluate(() => window.cy.nodes().length);
+// test("new model", async ({ page }) => {
+//   await page.goto("http://127.0.0.1:3245/");
+//   await page.getByRole("button", { name: "File" }).click();
+//   await page.getByRole("link", { name: "New Model" }).click();
+//   await page.getByText("OK").click();
+//   const numberOfNodes = await page.evaluate(() => window.cy.nodes().length);
 
-  // Use Jest to make the assertion that the array has length 0
-  expect(numberOfNodes).toBe(0);
-  await expect(page.getByTestId("result-text")).toContainText("Command");
+//   // Use Jest to make the assertion that the array has length 0
+//   expect(numberOfNodes).toBe(0);
+//   await expect(page.getByTestId("result-text")).toContainText("Command");
+// });
+
+// test("new model", async ({ page }) => {
+//   await page.goto("http://127.0.0.1:3245/");
+//   await page.getByRole("button", { name: "File" }).click();
+//   await page.getByRole("link", { name: "New Model" }).click();
+//   await page.getByText("OK").click();
+//   const numberOfNodes = await page.evaluate(() => window.cy.nodes().length);
+
+//   // Use Jest to make the assertion that the array has length 0
+//   expect(numberOfNodes).toBe(0);
+//   await expect(page.getByTestId("result-text")).toContainText("Command");
+// });
+
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
 });
--- src/vite.config.js
+++ src/vite.config.js
@@ -5,9 +5,9 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    minify: true,
+    minify: false,
     outDir: "../inst/www",
-    sourcemap: false,
+    sourcemap: true,
     emptyOutDir: true,
   },
   //base: "",
