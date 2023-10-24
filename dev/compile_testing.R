149d8a2621cc5bb5e28644d10026bb765fe9197e
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2023-10-24 16:40:18 +0200

    playwright testing bugfix attempt

--- dev/compile_testing.R
+++ dev/compile_testing.R
@@ -15,5 +15,11 @@ golem::detach_all_attached()
 # Document and reload your package
 #roxygen2::roxygenise()
 pkgload::load_all()
+library(lavaan)
+HS.model <- ' visual  =~ x1 + x2 + x3
+              textual =~ x4 + x5 + x6
+              speed   =~ x7 + x8 + x9 '
+
+fit <- cfa(HS.model, data = HolzingerSwineford1939)
 options(shiny.port = 3245)
-start_gui()
+start_gui(fit)
