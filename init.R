660d9e92fd9af2ce71cc2c7183d1d7ef419b65bb
Julian Karch <karchjd@fsw.leideuniv.nl>
2023-12-19 21:45:59 +0100

    added tools in init

--- init.R
+++ init.R
@@ -1,3 +1,4 @@
-install.packages("pkgload")
-install.packages("remotes")
+if (!require(pkgload)) install.packages("pkgload")
+if (!require(remotes)) install.packages("remotes")
+if (!require(tools)) install.packages("tools")
 remotes::install_deps()
