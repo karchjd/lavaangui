e5e3066b8bb8bd9be0f7199cdae8612d0a373866
Julian Karch <karchjd@fsw.leideuniv.nl>
2024-01-25 16:39:04 +0100

    second attempt

--- dev/start_for_tests.R
+++ dev/start_for_tests.R
@@ -1,14 +1,3 @@
-recompile_front <- T
-
-if (recompile_front){
-  # Compile Svelte front end 
-  ret_val <- system("cd src && npm run build:deploy")
-  if (ret_val != 0) {
-    stop("Failed to compile Svelte front end.")
-  }  
-}
-
-
 # Detach all loaded packages and clean your environment
 golem::detach_all_attached()
 
