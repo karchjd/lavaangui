549724f5e0665bbf1509b7ce3ebfa9e2a2138066
Julian Karch <karchjd@fsw.leideuniv.nl>
2024-03-22 14:22:09 +0100

    debugged file menu

--- R/serverDataUploader.R
+++ R/serverDataUploader.R
@@ -79,15 +79,18 @@ read_auto <- function(filepath) {
 serverDataUploader <- function(id, getData) {
   moduleServer(id, function(input, output, session) {
     data <- reactiveVal(NULL)
+    import <- logical(1)
     observeEvent(input$fileInput, {
       if (is.null(input$fileInput$content)) {
         data(list(df = read_auto(input$fileInput$datapath), name = input$fileInput$name))
+        import <- FALSE
       } else {
         content <- input$fileInput$content
         decoded <- base64enc::base64decode(content)
-        data <- (list(df = readr::read_csv(decoded), name = "data.csv"))
+        data(list(df = readr::read_csv(decoded), name = "data.csv"))
+        import <- TRUE
       }
-      propagateData(data(), session)
+      propagateData(data(), session, import)
     })
     data
   })
