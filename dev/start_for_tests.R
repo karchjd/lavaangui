5e302372d3094ee2affd19a0146a9c37cac63160
Julian Karch <karchjd@fsw.leideuniv.nl>
2024-05-16 00:22:25 +0200

    extended e2e tests

--- dev/start_for_tests.R
+++ dev/start_for_tests.R
@@ -7,17 +7,6 @@ if (!requireNamespace("pkgload", quietly = TRUE)) {
 }
 
 golem::detach_all_attached()
-
-print("hallo")
-# Document and reload your package
-# roxygen2::roxygenise()
 pkgload::load_all()
-library(lavaan)
-HS.model <- " visual  =~ x1 + x2 + x3
-              textual =~ x4 + x5 + x6
-              speed   =~ x7 + x8 + x9 "
-
-fit <- cfa(HS.model, data = HolzingerSwineford1939)
 options(shiny.port = 3245)
-print("Starting server...")
-start_gui(fit)
+start_gui()
