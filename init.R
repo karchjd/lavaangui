cda6313c49f25d535b333a8c653d531610db5bda
Julian Karch <karchjd@fsw.leideuniv.nl>
2024-05-15 18:15:41 +0200

    kicked semPlot in the hope for reasonable build time on heroku

--- init.R
+++ init.R
@@ -1,4 +1,2 @@
-if (!require(pkgload)) install.packages("pkgload")
 if (!require(remotes)) install.packages("remotes")
-if (!require(semPlot)) install.packages("semPlot", dependencies = FALSE)
 remotes::install_deps()
