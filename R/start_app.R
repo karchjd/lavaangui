29fdcdad2006ebe8a344535e4cf4c203bebbab23
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2024-09-10 10:44:47 +0200

    removed version print at startup, lintr start_app

--- R/start_app.R
+++ R/start_app.R
@@ -3,13 +3,12 @@
 #' @importFrom igraph graph.edgelist layout.reingold.tilford
 
 start_app <- function(fit = NULL, full, where) {
-  
   ## import model if present
   if (!is.null(fit)) {
     varNames <- lavaanNames(fit, type = "ov")
     factNames <- lavaanNames(fit, type = "lv")
     factNames <- factNames[!factNames %in% varNames]
-    if (lavInspect(fit, "ngroups") == 1){
+    if (lavInspect(fit, "ngroups") == 1) {
       df <- tryCatch(
         {
           as.data.frame(lavInspect(fit, what = "data"))
@@ -18,20 +17,20 @@ start_app <- function(fit = NULL, full, where) {
           message("Data not available only importing model")
           return(NULL)
         }
-      )  
-    }else{
-      if(full){
+      )
+    } else {
+      if (full) {
         stop("Multipe group models are currently not supported. But you can plot your model using plot_interactive")
       }
-      df = NULL
+      df <- NULL
     }
     parTable <- parTable(fit)
-    parTable <- parTable[!parTable$op%in%c(':=','<','>','==','|','<', '>'),]
+    parTable <- parTable[!parTable$op %in% c(":=", "<", ">", "==", "|", "<", ">"), ]
     importedModel <- list(obs = varNames, latent = factNames, parTable = parTable, df = df, fit = fit)
-  }else{
+  } else {
     importedModel <- NULL
   }
-  
+
   ## define server, here because we need to pass model, and full
   lavaan_gui_server <- function(input, output, session) {
     # reactive vals
@@ -47,19 +46,17 @@ start_app <- function(fit = NULL, full, where) {
         fit(importRes$fit)
       }
     }
-    
+
     session$sendCustomMessage("version", message = utils::packageVersion("lavaangui"))
-    print(utils::packageVersion("lavaangui"))
-    
-    
+
     ## View data
     serverDataViewer("dataViewer", getData)
-    
+
     ## seems needed but probably can be done better
     observeEvent(input$sendnames, {
       session$sendCustomMessage("columnames", message = input$newnames)
     })
-    
+
     ## Upload data
     x <- serverDataUploader("dataUpload")
     data_react <- reactive({
@@ -71,7 +68,7 @@ start_app <- function(fit = NULL, full, where) {
         return(NULL)
       }
     })
-    
+
     ## rename data
     getData <- reactive({
       local_data <- data_react()
@@ -80,27 +77,27 @@ start_app <- function(fit = NULL, full, where) {
       }
       return(local_data$df)
     })
-    
+
     ## layout
     serverLayout("layout", fit, full, imported)
-    
+
     ## main server for running ladan
     serverLavaanRun("run", to_render, forceEstimateUpdate, getData, fit)
-    
+
     serverEstimateUpdater("ests", forceEstimateUpdate, fit, to_render)
-    
+
     serverResultUpdater("res", to_render)
-    
+
     serverDownloader("down", getData)
-    
+
     extendResultsServer("extend", fit)
-    
+
     # showing help leave as is
     observeEvent(input$show_help, {
       to_render(help_text)
     })
   }
-  
+
   ## start app
   ui_loc <- system.file("www/index.html", package = "lavaangui")
   addResourcePath("assets", system.file("www/assets", package = "lavaangui"))
@@ -114,10 +111,9 @@ start_app <- function(fit = NULL, full, where) {
     runApp(app, port = as.numeric(Sys.getenv("PORT")), host = "0.0.0.0")
   } else if (where == "gadget") {
     runGadget(app, viewer = dialogViewer("lavaangui", width = 10^3, height = 10^3))
-  } else if (where == "shinyapps.io"){
+  } else if (where == "shinyapps.io") {
     return(app)
-  }else{
+  } else {
     stop("Invalid where argument")
   }
-
 }
