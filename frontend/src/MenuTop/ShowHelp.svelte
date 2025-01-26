86e296e11b2cff36287a7c75304cd095c5f8ed37
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2025-01-26 20:21:44 +0100

    added cite info

--- frontend/src/MenuTop/ShowHelp.svelte
+++ frontend/src/MenuTop/ShowHelp.svelte
@@ -15,6 +15,10 @@
       name: "About",
       action: openAbout,
     },
+    {
+      name: "Cite",
+      action: openCite,
+    },
   ];
 
   function showCommands() {
@@ -23,13 +27,20 @@
   }
 
   function openManual() {
-    window.open("https://doi.org/10.31234/osf.io/f4ary", "_blank");
+    window.open("https://doi.org/10.1080/10705511.2024.2420678", "_blank");
   }
 
   function openAbout() {
     bootbox.alert({
       title: "About Lavaangui",
       message: `Version: ${version}<br> Author: Julian D. Karch<br> Email: j.d.karch@fsw.leidenuniv.nl <br> lavaangui is BETA software. Please report any bugs at https://github.com/karchjd/lavaangui/issues.`,
+    });
+  }
+
+  function openCite() {
+    bootbox.alert({
+      title: "How to Cite",
+      message: `If you use lavaangui, please consider citing the following paper:<br> Karch, J. D. (2025). lavaangui: A Web-Based Graphical Interface for Specifying Lavaan Models by Drawing Path Diagrams. <i>Structural Equation Modeling: A Multidisciplinary Journal.</i> <a href="https://doi.org/10.1080/10705511.2024.2420678" target="_blank">https://doi.org/10.1080/10705511.2024.2420678</a>`,
     });
   }
 
