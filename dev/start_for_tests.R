8930f50a00f0c8c8669f177c37cfe219d084723b
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2026-03-10 14:40:15 +0100

    start for tests recompiles now

--- dev/start_for_tests.R
+++ dev/start_for_tests.R
@@ -1,9 +1,13 @@
-# Detach all loaded packages and clean your environment
-if (!requireNamespace("golem", quietly = TRUE)) {
-  install.packages("golem")
-}
-if (!requireNamespace("pkgload", quietly = TRUE)) {
-  install.packages("pkgload")
+
+rebuild_frontend <- TRUE
+build_target <- "deplot"
+
+if (rebuild_frontend) {
+  build_cmd <- if (build_target == "dev") "build:dev" else "build:deploy"
+  ret_val <- system2("npm", c("--prefix", "frontend", "run", build_cmd))
+  if (ret_val != 0) {
+    stop("Failed to compile Svelte front end.")
+  }
 }
 
 golem::detach_all_attached()
