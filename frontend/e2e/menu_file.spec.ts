6ba9a77627bb4fa09eb2c7d165d239e8fed4ebfa
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2026-04-07 16:53:59 +0200

    updated playwright tests for export from lavaangui

--- frontend/e2e/menu_file.spec.ts
+++ frontend/e2e/menu_file.spec.ts
@@ -84,8 +84,9 @@ test("Remove Data", async ({ page }) => {
 test("Export PNG", async ({ page }) => {
   await page.getByRole("button", { name: "File" }).click();
   await page.waitForTimeout(500);
-  const downloadPromise = page.waitForEvent("download");
   await page.getByRole("link", { name: "PNG", exact: false }).click();
+  const downloadPromise = page.waitForEvent("download");
+  await page.getByRole("button", { name: "OK" }).click();
   const download = await downloadPromise;
   expect(download.suggestedFilename()).toBe("model.png");
 });
@@ -93,8 +94,9 @@ test("Export PNG", async ({ page }) => {
 test("Export JPG", async ({ page }) => {
   await page.getByRole("button", { name: "File" }).click();
   await page.waitForTimeout(500);
-  const downloadPromise = page.waitForEvent("download");
   await page.getByRole("link", { name: "JPG", exact: false }).click();
+  const downloadPromise = page.waitForEvent("download");
+  await page.getByRole("button", { name: "OK" }).click();
   const download = await downloadPromise;
   expect(download.suggestedFilename()).toBe("model.jpg");
 });
@@ -103,8 +105,9 @@ test("Export JPG", async ({ page }) => {
 test("Export SVG", async ({ page }) => {
   await page.getByRole("button", { name: "File" }).click();
   await page.waitForTimeout(500);
-  const downloadPromise = page.waitForEvent("download");
   await page.getByRole("link", { name: "SVG", exact: false }).click();
+  const downloadPromise = page.waitForEvent("download");
+  await page.getByRole("button", { name: "OK" }).click();
   const download = await downloadPromise;
   expect(download.suggestedFilename()).toBe("model.svg");
 });
@@ -113,8 +116,9 @@ test("Export SVG", async ({ page }) => {
 test("Export PDF", async ({ page }) => {
   await page.getByRole("button", { name: "File" }).click();
   await page.waitForTimeout(500);
-  const downloadPromise = page.waitForEvent("download");
   await page.getByRole("link", { name: "PDF", exact: false }).click();
+  const downloadPromise = page.waitForEvent("download");
+  await page.getByRole("button", { name: "OK" }).click();
   const download = await downloadPromise;
   expect(download.suggestedFilename()).toBe("model.pdf");
 });
