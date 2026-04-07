07b8ab073d5d0853b3db7a7c32a8811b48645539
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2026-04-07 16:38:25 +0200

    export message

--- R/serverImageExporter.R
+++ R/serverImageExporter.R
@@ -4,6 +4,7 @@ serverImageExporter <- function(id) {
             req(input$image)
             png_raw_data <- base64enc::base64decode(input$image$data)
             writeBin(png_raw_data, input$image$filepath)
+            message(sprintf("Saved image to %s", input$image$filepath))
             session$sendCustomMessage("close", "")
         })
     })
